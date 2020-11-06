0e01ffa2885e6f365eeae8b98792dde8fc62fc2e
ellaisa <ellaisa@bk.ru>
2020-11-06 15:03:18 +0300

    Section 9

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLable: UILabel!
+    @IBOutlet weak var questionLable: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    /*
+     let     quiz = [["Four + Two is equal to Six", "True"],
+                    ["Five - Three is greater than One", "True"],
+                    ["Three + Eigth is less than Ten", "False"]
+    ]
+
+or     */
+    /* (2)
+    let     quiz = [Question(q: "A slug's blood is green.", a: "True"),
+                    Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+                    Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+                    Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+                    Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+                    Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+                    Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+                    Question(q: "Google was originally called 'Backrub'.", a: "True"),
+                    Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+                    Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+                    Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+                    Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+
+    ]
+ */
+ //   var     questionNumber = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+        //progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        // or (1)
+        //let actualQuestion = quiz[questionNumber]
+        //(2)
+        //let actualAnswer = quiz[questionNumber].answer
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        //or (1)
+        //let actualAnswer = actualQuestion.answer
+        
+        //if userAnswer == actualAnswer{
+        if userGotItRight{
+            sender.backgroundColor = UIColor.green
+        }else{
+            sender.backgroundColor = UIColor.red
+        }
+      /*
+        if questionNumber < quiz.count - 1{ // or questionNumber < quiz.count
+            questionNumber += 1
+        }else{
+            questionNumber = 0
+        }
+ */
+        quizBrain.nextQuestion()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI(){
+        //questionLable.text = quiz[questionNumber].text
+        questionLable.text = quizBrain.getQuestionText()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        //progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+        progressBar.progress = quizBrain.getProgress()
+        scoreLable.text = "Score: \(quizBrain.getScore())"
+    }
+}
+
